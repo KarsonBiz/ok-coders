130312e3283c900443f05ace0044b9d7bc034421
Karson Bizzell <99280654+KarsonBiz@users.noreply.github.com>
2022-05-10 15:33:57 -0500

    Garage Sale Project Update

--- Module 2/Garage Sale Project/index.js
+++ Module 2/Garage Sale Project/index.js
@@ -20,9 +20,18 @@ const onPageLoad = () => {
     setTotalItemsInCart();
 }
 
+const disable = (shouldDisable) => {
+    if (shouldDisable) {
+        return 'disabled';
+    }
+    return '';
+}
+
 // Render Products
 function renderProducts() {
     products.forEach((product) => {
+        const productIsInCart = cart.some(cartItem => cartItem.id === product.id);
+        console.log("🚀 ~ file: index.js ~ line 27 ~ products.forEach ~ productIsInCart", productIsInCart)
         productsEl.innerHTML += `
             <div class="col-lg-4 col-md-6 d-flex justify-content-center mb-5">
                 <div class="card">
@@ -33,7 +42,7 @@ function renderProducts() {
                         <h5 class="card-title header-3">${product.name}</h5>
                         <p class="card-text paragraph-4">${product.description}</p>
                         <p>$${product.price}</p>
-                        <button class="btn btn-outline-secondary btn-green align-self-end" onclick="addToCart(${product.id})">Add to Cart</button>
+                        <button id="${product.id}" class="btn btn-outline-secondary btn-green align-self-end" onclick="addToCart(${product.id})" ${disable(productIsInCart)}>Add to Cart</button>
                     </div>
                 </div>   
             </div>    
@@ -54,7 +63,8 @@ function addToCart(id) {
         const updatedCart = changeNumberOfUnits(id)
         return updateCart(updatedCart);
     }
-
+    const addProductButton = document.getElementById(id);
+    addProductButton.disabled = true;
     const item = products.find((product) => product.id === id);
 
     const newItem = {
