cd68dbcafc9427b7a69456f7d5360961db9b6d25
Karson Bizzell <99280654+KarsonBiz@users.noreply.github.com>
2022-05-11 18:36:55 -0500

    Day 3

---
+++ Module 3/Day 3/test.js
@@ -0,0 +1,45 @@
+// Algorithm Reverse a String
+
+function reverseString(str) {
+    const returnString = ''
+    for (let i = str.length - 1; i < 0; i++) {
+        returnString +=str(i)
+    }
+    return returnString;
+}
+
+reverseString('hello');
+
+
+// Debugging a Program
+// console.log()
+let value = {};
+console.log(value);
+
+
+// debugger keyword
+debugger;  //used for browser debugging
+
+
+// types
+const str = '';
+typeof str;  // outputs "string"
+
+const num = 0;
+typeof num;  // outputs "number"
+
+
+// thruthy vs falsey
+// falsey
+underfined
+null0
+
+//thruthy
+123
+"test"
+{ }
+
+
+if (a === b) {
+    
+}
